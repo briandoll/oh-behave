e343f91c672449daeee87f9e923359eb714c6f5e
Brian Doll <brian@emphaticsolutions.com>
2010-02-28 22:01:38 -0800

    just feeling things out

--- lib/oh-behave.rb
+++ lib/oh-behave.rb
@@ -1,5 +1,31 @@
 require 'garb'
 
 module OhBehave
-  puts "Oh, Behave!"
+    
+  class TestSuite
+    cattr_accessor :email, :password, :profile, :authenticated
+    
+    def self.login
+      @@auth_token ||= begin
+        if self.email && self.password
+          Garb::Session.login(self.email, self.password)
+          authenticated = true
+        else
+          raise "Please set your Google Analytics email and password in your test suite."
+        end
+      end
+    end
+    
+    def self.site
+      @@site ||= begin
+        login
+        if self.profile
+          Garb::Profile.first(profile)
+        else
+          raise "Please set the profile of the Google Analytics site you'd like to test."
+        end
+      end
+    end
+    
+  end
 end
--- test/test_oh-behave.rb
+++ test/test_oh-behave.rb
@@ -1,7 +1,32 @@
 require 'helper'
 
 class TestOhBehave < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  
+  include OhBehave
+  
+  context "A TestSuite without email and password set" do
+    setup do
+      TestSuite.email = nil
+      TestSuite.password = nil
+    end
+    should "fail when attempting to log in" do
+      assert_raise RuntimeError do
+        TestSuite.login
+      end
+    end
   end
+
+  context "A TestSuite with email and password set" do
+    setup do
+      TestSuite.email = 'test@test.net'
+      TestSuite.password = 'password'
+    end
+    should "be able to log in" do
+      # Auth error, since this is a bogus account
+      assert_raise Garb::AuthenticationRequest::AuthError do
+       TestSuite.login
+      end
+    end
+  end
+  
 end
